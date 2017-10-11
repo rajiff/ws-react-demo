e0615f48006eb8cd51ba7f4e954938e8dca89f39
Basavaraj K N <Basavaraj.K@Stackroute.in>
2017-10-11 19:22:43 +0530

    Intermediate commit

--- public/components/WebSocketClient.jsx
+++ public/components/WebSocketClient.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import io from 'socket.io-client';
+import P2PSocket from 'socket.io-p2p';
 
 export default class WebSocketClient extends React.Component {
 	constructor(props) {
@@ -12,46 +13,101 @@ export default class WebSocketClient extends React.Component {
 			myMessage: '',
 			error: undefined
 		}
-
-		this.handleChange = this.handleChange.bind(this);
-		this.sendMyMessage = this.sendMyMessage.bind(this);
-	}
+	} // end of constructor
 
 	componentDidMount() {
-		this.socket = io();
+		this.wsSocket = io();
 
-		this.socket.on('PONG', (newMsg) => {
+		let options = { autoUpgrade: false, numClients: 10, peerOpts: { numClients: 10} };
+
+		this.p2pSocket = new P2PSocket(this.wsSocket, options, () => {
+			console.log("New PEER ", this.p2pSocket.peerId);
+
+			this.p2pSocket.emit('event::onNewPEER', 'Hello there, I am ' + this.p2pSocket.peerId);
+		});
+
+		this.p2pSocket.on('ready', function() {
+			this.p2pSocket.usePeerConnection = true;
+  		this.p2pSocket.emit('peer-obj', { peerId: this.p2pSocket.peerId });
+		});
+
+		this.p2pSocket.on('peer-msg', function(data){
+  		console.log("Peer msg data: ", data);
+		});
+
+		this.p2pSocket.on('upgrade', function (data) {
+			console.log("Socket is upgrading to WebRTC with data: ", data);
+		});
+
+		this.p2pSocket.on('peer-error', function (err) {
+			console.log("Error: ", err);
+		});
+
+		this.p2pSocket.on('event::PONG', (newMsg) => {
 			newMsg = JSON.parse(newMsg);
+
 			// console.log('Got message ', newMsg);
-    		this.state.messages.push(newMsg);
-			this.setState({messages: this.state.messages});
-  		});
-	}
+    	let messages = this.state.messages;
+    	messages.push(newMsg);
+			this.setState({messages: messages});
+  	});
+
+  	this.p2pSocket.on('event::onToWebRTC', (newMsg) => {
+  		console.log("Switching to WebRTC..!");
+
+			this.upgradeToWebRTC();
+  	});
+	} // end of componentDidMount
 
 	componentWillUnmount() {
-	    if (!this.socket) return;
+    if (this.wsSocket) {
+    	try {
+    		this.wsSocket.close();
+    	} catch (e) {
+    		console.log('Error in closing web socket: ', err);
+    	}
+    }
 
-	    try {
-	    	this.socket.close();
-	    } catch (e) {
-	    	console.log('Error in closing socket: ', err);
-	    }
-  	}
+    if(this.p2pSocket) {
+    	try {
+    		this.p2pSocket.close();
+    	} catch (e) {
+    		console.log('Error in closing web socket: ', err);
+    	}
+    }
+	}
 
-	handleChange(event) {
+	handleNewMessage = (event) => {
 		this.setState({myMessage: event.target.value});
 	}
 
-	sendMyMessage() {
-		if(this.socket && this.state.myMessage) {
-			this.socket.emit('PING', ('New message @ browser ' + this.state.myMessage), function(result){
+	switchToWebRTC = () => {
+		console.log("Going to switch to WebRTC..!");
+
+		this.upgradeToWebRTC();
+
+		this.p2pSocket.emit('event::overToWebRTC', true);
+	}
+
+	upgradeToWebRTC = () => {
+		this.p2pSocket.upgrade();
+		// this.p2pSocket.useSockets = false;
+	}
+
+	sendMyMessage = () => {
+		if( (this.wsSocket && this.p2pSocket) && this.state.myMessage) {
+			console.log("Sending message..!");
+			this.p2pSocket.emit('event::PING', ('New message @ browser ' + this.state.myMessage));
+			/*this.p2pSocket.emit('event::PING', ('New message @ browser ' + this.state.myMessage), function(result){
 				// console.log('Test this', result);
 				if(!result) {
-					console.log('Error in sending websocket message ', result);
+					console.log('Error in sending message ', result);
 					this.setState({error: result});
 				}
 				console.log('message sent..!');
-			});
+			});*/
+		} else {
+			console.log(":-(");
 		}
 	}
 
@@ -63,9 +119,13 @@ export default class WebSocketClient extends React.Component {
 						hintText="Send message"
 						floatingLabelText="Send message"
 						value = {this.state.myMessage}
-						onChange = {this.handleChange}
+						onChange = {this.handleNewMessage}
 					/>
-					<RaisedButton label="Send" primary={true} onClick={this.sendMyMessage}/>
+				</div>
+				<div>
+					<RaisedButton label="Send" primary={true} onClick={this.sendMyMessage} style={{margin: '5px'}}/>
+
+					<RaisedButton label="Over WebRTC" primary={false} onClick={this.switchToWebRTC} style={{margin: '5px'}}/>
 				</div>
 				<div>
 					Messages({this.state.messages.length})
--- server/websocket.server.js
+++ server/websocket.server.js
@@ -1,16 +1,24 @@
 const socketIO = require('socket.io');
+const p2pSocketServer = require('socket.io-p2p-server').Server;
 
 const wsService = function(server) {
   let msgSeq = 0;
 
   const wsServer = socketIO(server);
+  wsServer.use(p2pSocketServer); //Attaching p2p server middleware
 
   wsServer.on('connection', function(clientSocket) {
     console.log('[*] Got new client socket connection ');
 
+    // p2pSocketServer(clientSocket);
+
+    clientSocket.on('event::onNewPEER', function(message) {
+      console.log('[ ^ ] New PEER: ', message);
+    });
+
     // PING is the event name
-    clientSocket.on('PING', function(message) {
-      console.log('received: %s', message);
+    clientSocket.on('event::PING', function(message) {
+      console.log('[ <- ] Received: ', message);
 
       let now = new Date();
       let msg = {
@@ -20,12 +28,25 @@ const wsService = function(server) {
       };
 
       // PONG is the event name
-      clientSocket.emit('PONG', JSON.stringify(msg));
+      // console.log("[ -> ] Sending message ");
+      // clientSocket.emit('PONG', JSON.stringify(msg));
+
+      // Send to all connected WS clients
+      console.log("[ => ] Broadcasting message ");
+      // wsServer.emit('event::PONG', JSON.stringify(msg));
+      clientSocket.broadcast.emit('event::PONG', JSON.stringify(msg));
+    });
+
+    // clientSocket.on('event::overToWebRTC', function(message) {
+    clientSocket.on('go-private', function(message) {
+      console.log("[ => ] Broadcasting to switch to WebRTC ");
+      clientSocket.broadcast.emit('event::onToWebRTC', JSON.stringify(message));
     });
 
     clientSocket.on('disconnect', function() {
       console.log('[*] Client socket disconnected ...!');
     });
+
   });
 }
 
